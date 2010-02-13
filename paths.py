82c24e066aa210ec833e4270ba49de4508017db7
Vasco Fernandes <vasco.box@gmail.com>
2010-02-13 19:51:10 +0000

    full html - mysql database - paths completed

--- paths.py
+++ paths.py
@@ -7,25 +7,42 @@ import re
 import cgi
 import cgitb
 import render
- 
-def dispatch(path, rules):
+
+cgitb.enable() 
+
+def dispatch(path, rules, vars):
     for rule in rules:
         dest = rule['dest']
         r = re.search(rule['re'], path)
         if(r):
-            dest(**r.groupdict())
+            vars.update(r.groupdict())
+            try:
+                dest(**vars)
+            except TypeError:
+                render.page_404()
             return
     render.page_404()
  
  
-cgitb.enable() 
+
 form = cgi.FieldStorage()
 path = ''
 if 'path' in form:
     path = form['path'].value
 
+vars = {}
+for var in form:
+    if var != 'path':
+        vars[var] = form[var].value
+
+
 rules = [ {'re': r'^$', 'dest': render.main},
           {'re': r'^page/(?P<page>\d+)', 'dest': render.main},
-          {'re': r'^d/(?P<dedication>\d+)', 'dest': render.single}]
+          {'re': r'^d/(?P<d_id>\d+)', 'dest': render.single},
+          {'re': r'^feed', 'dest': render.feed},
+          {'re': r'^add-dedication', 'dest': render.add_dedication},
+          {'re': r'^delete/(?P<id>\d+)/(?P<secret>\d+)', 'dest': render.del_dedication}]
 
-dispatch(path, rules)
+dispatch(path, rules, vars)
+#render.send_html(vars)
+
